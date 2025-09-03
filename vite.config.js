c69fc77aafdd39aa5ab08c28d37fa233f1d805bd
eduardojacomo <eduardo.jacomo@gmail.com>
2025-09-03 09:47:58 -0300

    fix2 build

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { build, defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/eduardojacomo.github.io/',
+  // Use a raiz '/' para o base path, já que o nome do repositório
+  // é igual ao domínio do GitHub Pages.
+  base: '/',
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@fortawesome': fileURLToPath(new URL('./node_modules/@fortawesome', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
