6717eef0aff10bc7820b6abd37ff52893d64270f
eduardojacomo <eduardo.jacomo@gmail.com>
2026-02-19 22:20:56 -0300

    update viteconfig

--- vite.config.js
+++ vite.config.js
@@ -3,27 +3,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/portifolio/', // 👈 nome exato do repositório no GitHub
+  base: '/', // 👈 AJUSTE CONFORME EXPLIQUEI
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  optimizeDeps: {
-    include: [
-      '@fortawesome/fontawesome-svg-core',
-      '@fortawesome/free-solid-svg-icons',
-      '@fortawesome/vue-fontawesome'
-    ]
-  },
-  build: {
-    rollupOptions: {
-      // 👇 força incluir no bundle final
-      external: [],
-    },
-    commonjsOptions: {
-      include: [/node_modules/],
     }
   }
 })
